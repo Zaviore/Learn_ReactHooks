9f723c8382d8b5ed0a4a76ea1f61a3c25af2444e
zamhadi21 <zamhadi21@gmail.com>
2020-05-22 01:27:54 +0700

    Css v2

--- src/component/IngredientList.js
+++ src/component/IngredientList.js
@@ -5,7 +5,7 @@ function IngredientList({ ingredients }) {
   const ingredientElement = ingredients.map((ingredient) => {
     return <Incredient key={ingredient.id} {...ingredient} />;
   });
-  return <div>{ingredientElement}</div>;
+  return <div className="ingredient-grid">{ingredientElement}</div>;
 }
 
 export default IngredientList;
--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -4,30 +4,32 @@ import IngredientList from "./IngredientList";
 function Recipe(props) {
   const { name, cookTime, servings, instructions, ingredients } = props;
   return (
-    <div>
-      <div>
-        <h2>{name}</h2>
+    <div className="recipe">
+      <div className="recipe-header">
+        <h2 className="recipe-title">{name}</h2>
       </div>
       <div>
-        <button>Edit </button>
-        <button>Delete</button>
+        <button className="btn btn--primary mr-1">Edit </button>
+        <button className="btn btn--dagger">Delete</button>
       </div>
-      <div>
-        <span>Cook Time : </span>
-        <span>{cookTime}</span>
+      <div className="recipe-row">
+        <span className="recipe-label">Cook Time : </span>
+        <span className="recipe-value">{cookTime}</span>
       </div>
-      <div>
-        <span>Serving : </span>
-        <span>{servings}</span>
+      <div className="recipe-row">
+        <span className="recipe-label">Serving : </span>
+        <span className="recipe-value">{servings}</span>
       </div>
-      <div>
-        <span>Intructions : </span>
-        <span>{instructions}</span>
+      <div className="recipe-row">
+        <span className="recipe-label">Intructions : </span>
+        <span className="recipe-value recipe-instructions recipe-value-indented">
+          {instructions}
+        </span>
       </div>
 
-      <div>
-        <span>Ingredient : </span>
-        <span>
+      <div className="recipe-row">
+        <span className="recipe-label">Ingredient : </span>
+        <span className="recipe-value recipe-value-indented">
           <IngredientList ingredients={ingredients} />
         </span>
       </div>
